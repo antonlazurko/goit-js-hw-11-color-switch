47075f82f928588b045cd78e5de04c75e768a46d
antonlazurko <lazurkoanton87@gmail.com>
2020-10-20 22:23:19 +0300

    делает черновик

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('button');
 const startBtn = buttons[0];
 const stopBtn = buttons[1];
 const body = document.querySelector('body');
-startBtn.addEventListener('click', onStartBtnClick);
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -11,20 +11,25 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
 let timeoutId = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+//функция генерации случайногочисла дляиндекса массива цветов
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//функция создания инлайна-стиля для body
+function colorChange(indexNumber) {
+  console.log(colors[indexNumber]);
+  return (body.style.backgroundColor = colors[indexNumber]);
+}
+
 function onStartBtnClick() {
   console.log('Start');
-  //функция генерации случайногочисла дляиндекса массива цветов
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  //функция создания инлайна-стиля для body
-  function colorChange(indexNumber) {
-    console.log(colors[indexNumber]);
-    console.log(colors.length);
 
-    return (body.style.backgroundColor = colors[indexNumber]);
-  }
   //включаем интервал
   timeoutId = setInterval(
     colorChange,
@@ -35,6 +40,7 @@ function onStartBtnClick() {
   stopBtn.addEventListener('click', onStopBtnClick);
   startBtn.removeEventListener('click', onStartBtnClick);
 }
+
 function onStopBtnClick() {
   console.log('Stop');
   // прерываем интервал
